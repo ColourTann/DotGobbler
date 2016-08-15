ab5aacc75e848f726074a23200076055b08c73ed
tann <tann@tann.space>
2016-08-15 23:26:51 +0100

    going to bed

---
+++ Assets/code/scripts/S_Ability.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class S_Ability : MonoBehaviour {
+    int pips;
+    bool active;
+    AbilityType type;
+    public enum AbilityType {
+        Move3
+    }
+    public void Toggle() {
+        active = !active;
+        GetComponent<SpriteRenderer>().color = active ? Colours.LIGHT : Colours.RED;
+    }
+
+    public void Activate() {
+      //  Game.Get().level.SetState();
+    }
+
+    public void Deplete() {
+        pips--;
+    }
+
+    public bool IsAvailable() {
+        return pips > 0;
+    }
+
+    public void init(int pips, AbilityType type) {
+        this.type = type;
+        this.pips = pips;
+    }
+
+
+}
